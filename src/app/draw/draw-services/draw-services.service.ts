5227ae0022b225c21525af6bf38f5f994a6b3e4a
Paul Gouin <paul.gouin@scalian.com>
2022-10-19 11:11:25 +0200

    Refacto draw-services

--- src/app/draw/draw-services/draw-services.service.ts
+++ src/app/draw/draw-services/draw-services.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { fabric } from 'fabric';
 
 @Injectable({
   providedIn: 'root',
@@ -8,10 +9,24 @@ export class DrawServicesService {
   private readonly _penState$: BehaviorSubject<boolean> =
     new BehaviorSubject<boolean>(false);
 
+  private _canvasFabric!: fabric.Canvas;
+
   public get penState(): BehaviorSubject<boolean> {
     return this._penState$;
   }
 
-  togglePen: Function = (): void =>
+  public initCanvas: Function = (): void => {
+    this._canvasFabric = new fabric.Canvas('draw-space', {
+      backgroundColor: 'lightgrey',
+      selection: false,
+      preserveObjectStacking: true,
+      width: 800,
+      height: 600,
+    });
+  };
+
+  togglePen: Function = (): void => {
     this._penState$.next(!this._penState$.value);
+    this._canvasFabric.isDrawingMode = this._penState$.value;
+  };
 }
--- src/app/draw/draw-space/draw-space.component.ts
+++ src/app/draw/draw-space/draw-space.component.ts
@@ -1,48 +1,17 @@
-import {
-  Component,
-  ElementRef,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
-import { fabric } from 'fabric';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DrawServicesService } from '../draw-services/draw-services.service';
-import { Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-draw-space',
   templateUrl: './draw-space.component.html',
   styleUrls: ['./draw-space.component.scss'],
 })
-export class DrawSpaceComponent implements OnInit, OnDestroy {
+export class DrawSpaceComponent implements OnInit {
   @ViewChild('drawSpace') canvasRef!: ElementRef;
-
-  private _canvasFabric!: fabric.Canvas;
-
-  private _unsubscribe$: Subject<undefined> = new Subject<undefined>();
 
   constructor(private readonly drawServices: DrawServicesService) {}
 
   ngOnInit(): void {
-    this._canvasFabric = new fabric.Canvas('draw-space', {
-      backgroundColor: 'lightgrey',
-      selection: false,
-      preserveObjectStacking: true,
-      width: 800,
-      height: 600,
-    });
-    this.drawServices.penState
-      .pipe(
-        takeUntil(this._unsubscribe$), // Jusqu'à ce que l'événement ait été déclenché au moins une fois.
-        tap((state: boolean): void => console.log(state))
-      )
-      .subscribe((state: boolean): void => {
-        this._canvasFabric.isDrawingMode = state;
-      });
-  }
-
-  ngOnDestroy(): void {
-    this._unsubscribe$.next(undefined);
-    this._unsubscribe$.complete();
+    this.drawServices.initCanvas();
   }
 }
