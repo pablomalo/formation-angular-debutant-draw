c43b0cb35b55a586ed97bf3e3e8b5d4e54a643b7
Paul Gouin <paul.gouin@scalian.com>
2022-10-20 09:50:55 +0200

    Install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './root-component/app.component';
 import { DrawModule } from './draw/draw.module';
 import { LifeCycleComponent } from './life-cycle/life-cycle.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, LifeCycleComponent],
-  imports: [BrowserModule, AppRoutingModule, DrawModule],
+  imports: [BrowserModule, AppRoutingModule, DrawModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
